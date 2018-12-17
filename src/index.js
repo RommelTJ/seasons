66fb3893a07518028a89ee597b39e242fa14c361
Rommel Rico <me@rommelrico.com>
2018-12-16 18:48:28 -0800

    Commenting out the functional component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Functional Component.
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    );
-
-    return <div>Latitude: </div>;
-
-};
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (error) => console.log(error)
+//     );
+//
+//     return <div>Latitude: </div>;
+//
+// };
 
 // Equivalent Class-Based Component.
 class App extends React.Component {
