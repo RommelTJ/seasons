ef091e1dd3ec95a07acdb2e9be77ff2ef3ea5337
Rommel Rico <me@rommelrico.com>
2018-12-21 07:00:59 -0800

    Grabbing the month

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    // Implement me
+    return "Winter";
+};
+
 const SeasonDisplay = (props) => {
-    return <div>Season Display: {props.lat}, {props.lon}</div>;
+    const season = getSeason(props.lat, new Date().getMonth());
+    return <div>Season Display: {season}</div>;
 };
 
 export default SeasonDisplay;
