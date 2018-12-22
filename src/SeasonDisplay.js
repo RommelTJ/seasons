05b476ccacfbc4910513def134cab20c3f2c9409
Rommel Rico <me@rommelrico.com>
2018-12-22 07:36:13 -0800

    Extra linebreak

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,8 +12,7 @@ const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === "winter" ? "Burr, it is chilly" : "Let's hit the beach";
     const icon = season === "winter" ? "snowflake" : "sun";
-
-
+    
     return (
         <div>
             <i className={`${icon} icon`} />
